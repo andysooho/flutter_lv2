991127f253edc2556152bc22227f68e83be223ec
andysooho <andysooho@gmail.com>
2023-07-25 00:17:17 +0900

    feat: 9. pagination UI
    pagination UI 적용

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -6,24 +6,71 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../provider/restaurant_provider.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(addListener);
+  }
+
+  void addListener() {
+    if (_scrollController.offset >
+        _scrollController.position.maxScrollExtent - 400) {
+      ref.read(restaurantProvider.notifier).paginate(
+            fetchMore: true,
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
+    // 완전 처음 로딩
     if (data is CursorPaginationLoading) {
       return const Center(child: CircularProgressIndicator());
     }
+
+    // 에러
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(
+          data.message,
+        ),
+      );
+    }
+
+    // CursorPagination, FetchMore, Refetching : meta, data 존재
 
     final cp = data as CursorPagination;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
-        itemCount: cp.data.length,
+        controller: _scrollController,
+        itemCount: cp.data.length + 1, // +1 : progress indicator
         itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: data is CursorPaginationFetchingMore
+                    ? const CircularProgressIndicator()
+                    : const Text('마지막 데이터 입니다.'),
+              ),
+            );
+          }
+
           final pItem = cp.data[index];
 
           return GestureDetector(
